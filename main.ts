116a4fd57b6ddf5f870107090f299543e87c49d6
Jonathan Malmaud <malmaud@gmail.com>
2024-12-26 14:52:56 -0600

    make command conditional

--- main.ts
+++ main.ts
@@ -1,21 +1,51 @@
-import { App, Editor, FuzzySuggestModal, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-import * as datefns from 'date-fns'
-import { extractFrontmatter, convertToMarkdown } from './frontmatter';
+import {
+	App,
+	Editor,
+	FuzzySuggestModal,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
+import * as datefns from "date-fns";
+import { extractFrontmatter, convertToMarkdown } from "./frontmatter";
 
 interface Status {
-    name: string;
+	name: string;
 }
 
-const STATUS_NAMES = ['complete', 'abandoned', 'backlog', 'in progress'] as const
-const STATUS_CHOICES = STATUS_NAMES.map(name=>{return {name: name}}) as Status[]
+const STATUS_NAMES = [
+	"complete",
+	"abandoned",
+	"backlog",
+	"in progress",
+] as const;
 
+const STATUS_CHOICES = STATUS_NAMES.map((name) => {
+	return { name: name };
+}) as Status[];
 
 export default class MyPlugin extends Plugin {
-
 	async onload() {
-
-		this.addCommand({id: 'modal', name: 'Set status', callback: ()=>this.openStatusChangeModal()})
-		this.addRibbonIcon('circle-check', 'Set status', ()=>this.openStatusChangeModal())
+		this.addCommand({
+			id: "modal",
+			name: "Set status",
+			checkCallback: (checking: boolean) => {
+				const file = this.app.workspace.getActiveFile();
+				if (!file) {
+					return false;
+				}
+				if (!checking) {
+					this.openStatusChangeModal();
+				}
+				return true;
+			},
+		});
+		this.addRibbonIcon("circle-check", "Set status", () =>
+			this.openStatusChangeModal()
+		);
 	}
 
 	openStatusChangeModal() {
@@ -23,53 +53,50 @@ export default class MyPlugin extends Plugin {
 	}
 
 	async setStatus(status: Status) {
-		console.log('doThing running')
-		const file = this.app.workspace.getActiveFile()
-		const vault = this.app.vault
+		console.log("doThing running");
+		const file = this.app.workspace.getActiveFile();
+		const vault = this.app.vault;
 
-		if(!file) {
-			new Notice("no file")
-			return
+		if (!file) {
+			new Notice("no file");
+			return;
 		}
 
-		const content = await vault.read(file)
+		const content = await vault.read(file);
 
-		console.log(`file contains ${content}`)
+		console.log(`file contains ${content}`);
 
-		const data = extractFrontmatter(content)
-		console.log(`frontmatter: ${JSON.stringify(data.frontmatter)}`)
+		const data = extractFrontmatter(content);
+		console.log(`frontmatter: ${JSON.stringify(data.frontmatter)}`);
 
-		data.frontmatter['status'] = status.name
+		data.frontmatter["status"] = status.name;
 
-		const formattedDate = datefns.format(new Date(), 'yyyy-MM-dd')
-		data.frontmatter['status date'] = formattedDate
+		const formattedDate = datefns.format(new Date(), "yyyy-MM-dd");
+		data.frontmatter["status date"] = formattedDate;
 
-		const markdown = convertToMarkdown(data)
-		await vault.modify(file, markdown)
+		const markdown = convertToMarkdown(data);
+		await vault.modify(file, markdown);
 	}
 
-	onunload() {
-
-	}
-
+	onunload() {}
 }
 
 class ChoiceModal extends FuzzySuggestModal<Status> {
-    onSubmit: (choice: Status)=>Promise<void>;
-    constructor(app: App, onSubmit: (choice: Status)=>Promise<void>) {
-        super(app);
-		this.onSubmit = onSubmit
-    }
+	onSubmit: (choice: Status) => Promise<void>;
+	constructor(app: App, onSubmit: (choice: Status) => Promise<void>) {
+		super(app);
+		this.onSubmit = onSubmit;
+	}
 
-    getItems(): Status[] {
-        return STATUS_CHOICES;
-    }
+	getItems(): Status[] {
+		return STATUS_CHOICES;
+	}
 
-    getItemText(status: Status): string {
-        return status.name
-    }
+	getItemText(status: Status): string {
+		return status.name;
+	}
 
-    onChooseItem(status: Status, evt: MouseEvent | KeyboardEvent) {
-        this.onSubmit(status)
-    }
+	onChooseItem(status: Status, evt: MouseEvent | KeyboardEvent) {
+		this.onSubmit(status);
+	}
 }
